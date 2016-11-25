5f4f82b72f9d39eacf9ea26fe2cacf3726ba1771
cramosk <cramo@gmx.de>
2016-11-25 17:55:52 +0100

    Update pcmda.py

--- pcmda.py
+++ pcmda.py
@@ -11,14 +11,7 @@ MIT-License, y'all. No need to go to jail for this, thank you very much.
 
 # Core libraries
 import math
-
-
-def GetDistance(x1, y1, x2, y2):
-    # Very basic. Fetch me the distance between two points. Pretty please... :'(
-    return d = math.sqrt(((x2-x1)**2)*((y2-y1)**2))
-
-def GetLinearActuatorFunction(x_start, y_start, x_end, y_end):
-    # Now we are getting lazy. I know, I know... We are starting out linear-ily. Ish.
+def GetSteps():
     While True:
         try:
             steps = int(raw_input("Enter the number of steps: "))
@@ -26,8 +19,48 @@ def GetLinearActuatorFunction(x_start, y_start, x_end, y_end):
         
         except:
             print "You need to supply this programm with the number of steps. Try again!"
+    return steps
+
+def GetDistance(x1, y1, x2, y2):
+    # Very basic. Fetch me the distance between two points. Pretty please... :'(
+    return d = math.sqrt(((x2-x1)**2)*((y2-y1)**2))
+
+def GetLinearActuatorFunction(x_start, y_start, x_end, y_end):
+    # Now we are getting lazy. I know, I know... We are starting out linear-ily. Ish.
+    
+    steps = GetSteps()
+    
     x, y, t = [], [], []
     
     for schritt in range(1,steps+1):
         t.append(steps)
-        x.append()
+        x.append(x_start+(x_end-x_start)*schritt/steps)
+        y.append(y_start+(y_end-y_start)*schritt/steps)
+    
+    f = {'t': t, 'x': x, 'y': y}
+    
+    return f
+
+def GetPeriodicActuatorFunction(x_start, y_start):
+    # And we are going... PERIODICALLY!
+    while True:
+        try:
+            startangle = int(raw_input("Choose the starting position:\n\n1 - Closest to reactant 1\n2 - Maximum distance from reactant1\n3 - Specify degree"))
+            break
+        except:
+            print "Try again. Choose 1, 2 or 3."
+    
+    if startangle == '1':
+        angle == 180
+    elif startangle =='2':
+        angle == 0
+    else:
+        while True:
+            try:
+                angle = int(raw_input("Degrees [°]: "))
+                angle = angle%360
+                break
+            except:
+                print "Invalid number. Try again"
+    steps = GetSteps()
+    
